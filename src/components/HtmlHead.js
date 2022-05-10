f4cf548f6fed81c8d8705ed70a91d01ce9eb800d
Daniel Bogod <daniel.bogod@historicengland.org.uk>
2022-05-10 10:40:40 +0100

    Move title to index.html

--- src/components/HtmlHead.js
+++ src/components/HtmlHead.js
@@ -1,4 +1,3 @@
-import { BROWSER_TITLE } from '../constants/strings';
 import { Helmet } from 'react-helmet-async';
 import { useContext } from 'react';
 import { ThemeContext } from '../context/Theme';
@@ -6,12 +5,7 @@ import { ThemeContext } from '../context/Theme';
 const HtmlHead = () => {
   const { currentTheme } = useContext(ThemeContext);
   return (
-    <Helmet 
-      htmlAttributes={{ lang: 'en-GB', 'data-theme': currentTheme }}>
-      <title>
-        {BROWSER_TITLE}
-      </title>
-    </Helmet>
+    <Helmet  htmlAttributes={{ lang: 'en-GB', 'data-theme': currentTheme }}/>
   );
 };
 
